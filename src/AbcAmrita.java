c0e979b6c166479eaa013a478ac06d31fde60193
AMRITA <patel.amrita4194@gmail.com>
2023-08-13 14:55:53 +0530

    2commit

---
+++ src/AbcAmrita.java
@@ -0,0 +1,8 @@
+
+public class AbcAmrita {
+
+	public static void main(String[] args) {
+		System.out.println(" hiiii");
+	}
+
+}
---
+++ src/Def.java
@@ -0,0 +1,11 @@
+
+public class Def {
+
+	public static void main(String[] args) {
+		System.out.println(" hiiii");
+		System.out.println(" hiiii");
+		System.out.println(" hiiii");
+		System.out.println(" hiiii");
+	}
+
+}

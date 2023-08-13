e8ce591eaf88f211cff3c9439bbd09d0ac30acd8
AMRITA <patel.amrita4194@gmail.com>
2023-08-13 14:41:10 +0530

    First commit

---
+++ src/Ammu2.java
@@ -0,0 +1,8 @@
+
+public class Ammu2 {
+
+	public static void main(String[] args) {
+		System.out.println(" Amrita");
+	}
+
+}
---
+++ src/Amu.java
@@ -0,0 +1,8 @@
+
+public class Amu {
+
+	public static void main(String[] args) {
+		System.out.println("Print");
+	}
+
+}
